87ba849404a5a549be1b8241b846a42a30abc827
BashirKad <bashir.kadri33@gmail.com>
2026-03-17 12:51:57 -0400

    small update to previous push
    Controls.js should send an AJAX request to NN.py, which should receive and print data

--- NN.py
+++ NN.py
@@ -8,8 +8,12 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-@app.route("/receiver", methods=["POST"])
+@app.route("/receiver", methods = ["POST"])
 
 def receiver():
     data = request.json
     print(data)
+    return jsonify({"Status" : "Successfully Received Message"})
+
+if __name__=="__main__":
+    app.run(host = "localhost", port = 8000)
