1c540529cb2c50dc5965a095e001e86ad8ab1e5d
BashirKad <bashir.kadri33@gmail.com>
2026-04-07 08:42:04 -0400

    tetsing stupid stuff

--- src/python/helpers.py
+++ src/python/helpers.py
@@ -1,8 +1,8 @@
 def helper(arr):
     
-    for i in range(9):
+    for i in range(4):
         if (not arr[i]["locked"]):
             print(i, ": ", arr[i]["color"])
         else:
             print("locked")
-    print("\n")
+    print("\n")             
--- src/python/receiver.py
+++ src/python/receiver.py
@@ -6,6 +6,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import helpers
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from PIL import Image
+import time
 
 app = Flask(__name__)
 CORS(app)
@@ -16,32 +22,42 @@ def receiver():
 
     print(data)
 
-    # blueFace = data["x+"]
-    # greenFace = data["x-"]
-    # whiteFace = data["y+"]
-    # yellowFace = data["y-"]
-    # redFace = data["z+"]
-    # orangeFace = data["z-"]
-
-    # print("BLUE")
-    # helpers.helper(blueFace)
-
-    # print("GREEN")
-    # helpers.helper(greenFace)
-
-    # print("WHITE")
-    # helpers.helper(whiteFace)
-
-    # print("YELLOW")
-    # helpers.helper(yellowFace)
-
-    # print("RED")
-    # helpers.helper(redFace)
-
-    # print("ORANGE")
-    # helpers.helper(orangeFace)
-
     return jsonify({"Status" : "Successfully Received Message"})
 
+def bootup(sizeID):
+    driver = webdriver.Chrome()
+    driver.get("https://bashirkad.github.io/CubeNN/")
+
+    time.sleep(1)
+    #get rid of pop-up
+    driver.find_element(By.TAG_NAME, "body").click()
+    driver.switch_to.active_element.send_keys(Keys.ESCAPE)
+
+    #select 3x3 for this project
+    button = driver.find_element(By.ID, sizeID)
+    button.click()
+
+    #double click 
+    time.sleep(0.3)
+    driver.find_element(By.TAG_NAME, "body").click()
+    driver.find_element(By.TAG_NAME, "body").click()
+    
+    time.sleep(5) #provide time for scramble to happen
+
+    driver.save_screenshot("cube.png")
+    time.sleep(0.1)
+    image = Image.open("cube.png")
+
+    coordList = [(1200, 380), (1320, 440), (1430, 500)]
+
+    for coordX, coordY in coordList:
+        pixel = image.getpixel((coordX, coordY))
+
+        print(f"Pixel at ({coordX}, {coordY}) = {pixel}")
+
+    input("")
+    
+
 if __name__=="__main__":
+    bootup("solobutton333")
     app.run(host = "localhost", port = 8000)
